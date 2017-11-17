1580505e48bcb06acb8d7def5a85703f010337c5
Andrea Zucchini <azga84@gmail.com>
2017-11-17 12:12:08 +0100

    implement a request message for the confirmation of the operations of the database

--- thingsverse-db/setup.js
+++ thingsverse-db/setup.js
@@ -1,9 +1,22 @@
 'use strict'
 const debug = require('debug')('thingsverse:db:setup')
+const inquirer = require('inquirer')
+const chalk = require('chalk')
 
 const db = require('./')
+const prompt = inquirer.createPromptModule()
 
 async function setup () {
+  const answer = await prompt([
+    {
+      type: 'confirm',
+      name: 'setup',
+      message: 'This will destroy your database, are you sure?'
+    }
+  ])
+  if (!answer.setup) {
+    return console.log('Nothing happened :)')
+  }
   const config = {
     database: process.env.DB_NAME || 'thingsverse',
     username: process.env.DB_USER || 'thingsverse',
@@ -20,7 +33,7 @@ async function setup () {
 }
 
 function handleFatalError (err) {
-  console.error(err.message)
+  console.error(`${chalk.red('[fatal error]')} ${err.message}`)
   console.error(err.stack)
   process.exit(1)
 }
