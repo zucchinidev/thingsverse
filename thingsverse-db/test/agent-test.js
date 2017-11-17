3c91ace459f75614ffc9b52620586acba979e570
Andrea Zucchini <azga84@gmail.com>
2017-11-17 13:48:40 +0100

    mocks and stubs with sinon and use the proxyquire

--- thingsverse-db/test/agent-test.js
+++ thingsverse-db/test/agent-test.js
@@ -1,16 +1,44 @@
 'use strict'
 
 const test = require('ava')
+const proxyquire = require('proxyquire')
+const sinon = require('sinon')
+
 let db = null
+let sandbox = null
 const config = {
   logging: () => {}
 }
 
+const MetricModelStub = {
+  belongsTo: sinon.spy()
+}
+
+let AgentModelStub = null
+
 test.beforeEach(async () => {
-  const setupDatabase = require('../')
+  sandbox = sinon.sandbox.create()
+  AgentModelStub = {
+    hasMany: sandbox.spy()
+  }
+  const setupDatabase = proxyquire('../', {
+    './models/agent': () => AgentModelStub,
+    './models/metric': () => MetricModelStub
+  })
   db = await setupDatabase(config)
+})
+
+test.afterEach(() => {
+  sandbox && sinon.sandbox.restore()
 })
 
 test('Agent', t => {
   t.truthy(db.Agent, 'Agent service should exists')
+})
+
+test.serial('Setup', t => {
+  t.true(AgentModelStub.hasMany.called, 'AgentModel.hasMany was executed')
+  t.true(AgentModelStub.hasMany.calledWith(MetricModelStub), 'Argument should be the MetricModel')
+  t.true(MetricModelStub.belongsTo.called, 'MetricModel.belongsTo was executed')
+  t.true(MetricModelStub.belongsTo.calledWith(AgentModelStub), 'Argument should be the AgentModel')
 })
