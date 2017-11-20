563ade36f24d90f73c70e898532b3e62829b933d
Andrea Zucchini <azga84@gmail.com>
2017-11-20 22:05:50 +0100

    metric fixtures

---
+++ thingsverse-db/test/fixtures/metric.js
@@ -0,0 +1,29 @@
+'use strict'
+const agentFixture = require('./agent')
+
+const metric = {
+  id: 1,
+  type: 'a',
+  value: '{data: "fakeData"}',
+  createdAt: new Date(),
+  UpdatedAt: new Date(),
+  agentId: agentFixture.first.id
+}
+
+const metrics = [
+  metric,
+  extend(metric, {id: 2, type: 'a', value: '{data: "fakeData-2"}', agentId: 2}),
+  extend(metric, {id: 3, type: 'b', value: 10, agentId: 3}),
+  extend(metric, {id: 4, type: 'c', value: true, agentId: 4})
+]
+
+function extend (obj, values) {
+  const clone = Object.assign({}, obj)
+  return Object.assign(clone, values)
+}
+
+module.exports = {
+  first: metric,
+  all: metrics,
+  getByTypeAgentId: (type, agentId) => metrics.filter(a => a.type === type && a.agentId === agentId)
+}
