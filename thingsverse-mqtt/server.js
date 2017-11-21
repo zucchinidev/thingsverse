ca32415cdaa748b78707465745af009b121480e1
Andrea Zucchini <azga84@gmail.com>
2017-11-22 00:22:58 +0100

    use thingsverse-db module to make the connection after initializing the MQTT server

--- thingsverse-mqtt/server.js
+++ thingsverse-mqtt/server.js
@@ -4,6 +4,10 @@ const debug = require('debug')('thingsverse:mqtt')
 const mosca = require('mosca')
 const redis = require('redis')
 const chalk = require('chalk')
+const db = require('thingsverse-db')
+
+let Metric = null
+let Agent = null
 
 const backend = {
   type: 'redis',
@@ -15,10 +19,20 @@ const mqttServerSettings = {
   port: 1883,
   backend
 }
+const dataBaseSettings = {
+  database: process.env.DB_NAME || 'thingsverse',
+  username: process.env.DB_USER || 'thingsverse',
+  password: process.env.DB_PASS || 'thingsverse',
+  host: process.env.DB_HOST || 'localhost',
+  dialect: 'postgres'
+}
 
 const server = new mosca.Server(mqttServerSettings)
 
-server.on('ready', () => {
+server.on('ready', async () => {
+  const services = await db(dataBaseSettings).catch(handleFatalError)
+  Agent = services.Agent
+  Metric = services.Metric
   console.log(`${chalk.green('[thingsverse-mqtt]')} server is running`)
 })
 
