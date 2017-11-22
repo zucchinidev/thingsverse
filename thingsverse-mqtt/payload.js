05fbb3b13baf50b26f8a50625bb57c2c9a65c91a
Andrea Zucchini <azga84@gmail.com>
2017-11-22 22:27:22 +0100

    parse payload module

---
+++ thingsverse-mqtt/payload.js
@@ -0,0 +1,16 @@
+function parsePayload (payload) {
+  if (payload instanceof Buffer) {
+    payload = payload.toString('utf8')
+  }
+
+  try {
+    payload = JSON.parse(payload)
+  } catch (err) {
+    payload = {}
+  }
+  return payload
+}
+
+module.exports = {
+  parsePayload
+}
