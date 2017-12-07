51c4a258839076d0a3c16781f2e40b0232b7cae4
Andrea Zucchini <azga84@gmail.com>
2017-12-07 19:54:25 +0100

    test payload module

---
+++ thingsverse-mqtt/test/payload-test.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const test = require('ava')
+const { parsePayload } = require('../src/payload')
+
+test('payload#parsePayload', t => {
+  let result = parsePayload('{"a":"a"}')
+  t.deepEqual(result, {a: 'a'}, 'Should be a valid object')
+  const buffer = Buffer.from('{"a":"a"}')
+  result = parsePayload(buffer)
+  t.deepEqual(result, {a: 'a'}, 'Should be a valid object')
+  result = parsePayload()
+  t.is(result, null, 'Should a null object')
+})
