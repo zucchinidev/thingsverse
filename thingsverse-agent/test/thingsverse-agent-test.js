f3c02afb84a494db83b1956543e146086f302555
Andrea Zucchini <azga84@gmail.com>
2017-12-10 10:39:09 +0100

    test method to send message

--- thingsverse-agent/test/thingsverse-agent-test.js
+++ thingsverse-agent/test/thingsverse-agent-test.js
@@ -9,6 +9,7 @@ let debug = null
 let sandbox = null
 let mqttStub = {}
 let uuidStub = {}
+let osStub = {}
 let clientStub = null
 
 test.beforeEach(async () => {
@@ -17,14 +18,17 @@ test.beforeEach(async () => {
   clientStub = {
     subscribe: sandbox.spy(),
     on: sandbox.stub(),
-    end: sandbox.spy()
+    end: sandbox.spy(),
+    publish: sandbox.spy()
   }
+  osStub.hostname = sandbox.stub().returns('stub-hostname')
   mqttStub.connect = sandbox.stub().returns(clientStub)
   uuidStub.v4 = sandbox.stub().returns('xxx')
   const ThingsverseAgent = proxyquire('../', {
     'debug': () => debug,
     'mqtt': mqttStub,
-    'uuid': uuidStub
+    'uuid': uuidStub,
+    'os': osStub
   })
 
   thingsverseAgent = new ThingsverseAgent({
@@ -87,4 +91,41 @@ test('thingsverserAgent@messageReceived', t => {
   t.true(onSendTopicSpy.called, `should emit a ${topic} event`)
   t.true(onSendTopicSpy.calledWith(payload), `should emit a ${topic} event`)
   thingsverseAgent.removeListener(topic, onSendTopicSpy)
+})
+
+test.serial('thingsverserAgent@emitMessage', async t => {
+  const now = new Date()
+  const originalPid = process.pid
+  const synchronousMetric = sinon.stub().returns('synchronousMetric')
+  const callbackMetric = sinon.stub().returns('callbackMetric')
+  const clock = sinon.useFakeTimers(now.getTime())
+  const asynchronousMetric = sinon.stub().returns(Promise.resolve('asynchronousMetric'))
+  thingsverseAgent.addMetric('rss', synchronousMetric)
+  thingsverseAgent.addMetric('promiseMetric', asynchronousMetric)
+  thingsverseAgent.addMetric('callbackMetric', callbackMetric)
+  Object.defineProperty(process, 'pid', {
+    value: 'fake-process'
+  })
+  const message = {
+    agent: {
+      username: 'things',
+      name: 'untitled',
+      hostname: 'stub-hostname',
+      pid: 'fake-process'
+    },
+    metrics: [
+      { type: 'rss', value: 'synchronousMetric' },
+      { type: 'promiseMetric', value: 'asynchronousMetric' },
+      { type: 'callbackMetric', value: 'callbackMetric' }
+    ],
+    timestamp: now.getTime()
+  }
+  thingsverseAgent.connect()
+  await thingsverseAgent.emitMessage()
+  t.true(clientStub.publish.called, 'should publish a new message')
+  t.true(clientStub.publish.calledWith('agent/message', JSON.stringify(message)), 'should publish a new message')
+  Object.defineProperty(process, 'pid', {
+    value: originalPid
+  })
+  clock.restore()
 })
