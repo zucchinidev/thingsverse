5043be24a21d33597d04227d2afc4139df133162
Andrea Zucchini <azga84@gmail.com>
2017-12-14 21:44:53 +0100

    Create api end-points

--- thingsverse-api/src/api.js
+++ thingsverse-api/src/api.js
@@ -8,43 +8,74 @@ const asyncify = require('express-asyncify')
 const config = require('./config')
 
 const AgentNotFoundError = require('./errors/agent-not-found-error')
+const MetricsNotFoundError = require('./errors/metrics-not-found-error')
 
 const api = asyncify(express.Router())
-let services, Agent, Metric
+let services
 
 api.use('*', async (req, res, next) => {
   if (!services) {
     debug('Connecting to database')
     try {
-      services = await db(config)
+      services = await db(config.db)
     } catch (err) {
       return next(err)
     }
-    Agent = services.Agent
-    Metric = services.Metric
   }
   next()
 })
 
-api.get('/agents', (req, res) => {
+api.get('/agents', async (req, res, next) => {
   debug('A request has come to /agents')
-  res.send({})
-})
-
-api.get('/agents/:uuid', (req, res, next) => {
-  const { uuid } = req.params
-  if (uuid !== 'yyy') {
-    return next(new AgentNotFoundError(uuid))
+  try {
+    const agents = await services.Agent.findConnected()
+    res.send(agents)
+  } catch (err) {
+    return next(err)
   }
-  res.send({ uuid })
 })
 
-api.get('/metrics/:uuid', (req, res) => {
-  const { uuid } = req.params
-  res.send({ uuid })
+api.get('/agents/:uuid', async (req, res, next) => {
+  try {
+    const { uuid } = req.params
+    debug(`request to /agents/${uuid}`)
+    const agent = await services.Agent.findByUuid(uuid)
+    if (!agent) {
+      return next(new AgentNotFoundError(uuid))
+    }
+    res.send(agent)
+  } catch (err) {
+    return next(err)
+  }
 })
 
-api.get('/metrics/:uuid/:type', (req, res) => {
+api.get('/metrics/:uuid', async (req, res, next) => {
+  try {
+    const { uuid } = req.params
+    debug(`request to /metrics/${uuid}`)
+    const metrics = await services.Metric.findByAgentUuid(uuid)
+    if (!metrics || metrics.length === 0) {
+      return next(new MetricsNotFoundError(uuid))
+    }
+    res.send({ metrics })
+  } catch (err) {
+    return next(err)
+  }
+})
+
+api.get('/metrics/:uuid/:type', async (req, res, next) => {
+  try {
+    const { uuid, type } = req.params
+    debug(`request to /metrics/${uuid}/${type}`)
+    const metrics = await services.Metric.findByTypeAgentUuid(type, uuid)
+    if (!metrics || metrics.length === 0) {
+      return next(new MetricsNotFoundError(uuid, type))
+    }
+    res.send({ metrics })
+  } catch (err) {
+    return next(err)
+  }
+
   const { uuid, type } = req.params
   res.send({ uuid, type })
 })
