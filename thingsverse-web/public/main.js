0d89b520b84526c40221c1e229c72a40f8840720
Andrea Zucchini <azga84@gmail.com>
2017-12-20 18:05:41 +0100

    subscribe to messages

--- thingsverse-web/public/main.js
+++ thingsverse-web/public/main.js
@@ -3,7 +3,15 @@
 const socket = io()
 
 socket.on('agent/message', payload => {
-  console.log(payload)
+  console.log('agent/message', payload)
+})
+
+socket.on('agent/connected', payload => {
+  console.log('agent/connected', payload)
+})
+
+socket.on('agent/disconnectedḉ', payload => {
+  console.log('agent/disconnected', payload)
 })
 
 setTimeout(() => {
