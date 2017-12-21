837ec77692d1fd295dccd6463245784e29e78d10
Andrea Zucchini <azga84@gmail.com>
2017-12-21 16:35:42 +0100

    module configuration

---
+++ thingsverse-web/src/config.js
@@ -0,0 +1,6 @@
+'use strict'
+
+module.exports = {
+  endpoint: process.env.API_ENDPOINT || 'http://localhost:3000',
+  apiToken: process.env.API_TOKEN || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRlc3QiLCJhZG1pbiI6dHJ1ZSwicGVybWlzc2lvbnMiOlsibWV0cmljczpyZWFkIiwiYWdlbnRzOnJlYWQiXSwiaWF0IjoxNTEzODU0NDc2fQ._ZDTrbiv_JVqA53IMBCIR3WYnkJ5awcc7LlyMCRB41U'
+}
