99777a2c56cf47e798d61d82759ebd9c41541f32
Andrea Zucchini <azga84@gmail.com>
2018-01-09 15:44:37 +0100

    draw metrics in dashboard

--- thingsverse-cli/thingsverse.js
+++ thingsverse-cli/thingsverse.js
@@ -11,7 +11,10 @@ const screen = blessed.screen()
 const agents = new Map()
 const agentMetrics = new Map()
 const extended = []
-
+const selectedNode = {
+  uuid: null,
+  type: null
+}
 /* eslint new-cap: off  */
 const grid = new contrib.grid({
   rows: 1,
@@ -98,7 +101,7 @@ function renderData () {
         type,
         metric: true
       }
-      const metricName = ` ${type}`
+      const metricName = ` ${type} ${' '.repeat(1000)} ${uuid}`
       treeData[title].children[metricName] = metric
     })
   }
@@ -106,11 +109,30 @@ function renderData () {
     extended: true,
     children: treeData
   })
+  renderMetric()
+}
+
+function renderMetric () {
+  const hasNotSelectedMetric = !selectedNode.uuid && !selectedNode.type
+  if (hasNotSelectedMetric) {
+    line.setData([{ x: [], y: [], title: '' }])
+    screen.render()
+    return
+  }
+
+  const metrics = agentMetrics.get(selectedNode.uuid)
+  const values = metrics[selectedNode.type]
+  const series = [{
+    title: selectedNode.type,
+    x: values.map(v => v.timestamp).slice(-10),
+    y: values.map(v => v.value).slice(-10)
+  }]
+  line.setData(series)
   screen.render()
 }
 
 tree.on('select', node => {
-  const { uuid } = node
+  const { uuid, type } = node
   const isAgent = node.agent
   if (isAgent) {
     if (node.extended) {
@@ -121,7 +143,13 @@ tree.on('select', node => {
         extended.splice(index, 1)
       }
     }
+    selectedNode.type = null
+    selectedNode.uuid = null
+    return
   }
+  selectedNode.type = type
+  selectedNode.uuid = uuid
+  renderMetric()
 })
 
 screen.key(['escape', 'q', 'C-c'], (ch, key) => {
